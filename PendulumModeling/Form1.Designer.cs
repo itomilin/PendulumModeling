d4d14aedfcce27aefe5172c78c31007596596f5d
Ilya Tomilin <tomilin.inbox@gmail.com>
2021-05-13 20:43:48 +0300

    init

---
+++ PendulumModeling/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace PendulumModeling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 16;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(214, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(752, 539);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 57);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(13, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 57);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 1;
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown1.Location = new System.Drawing.Point(18, 328);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(102, 20);
+            this.numericUpDown1.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(20, 208);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 192);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Длина нити:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(198, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "natural frequency (W):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 312);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Angle speed (omega):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 270);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "coefficient of friction (n):";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.DecimalPlaces = 2;
+            this.numericUpDown2.Value = 0.01M;
+            this.numericUpDown2.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown2.Location = new System.Drawing.Point(18, 286);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(102, 20);
+            this.numericUpDown2.TabIndex = 15;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.DecimalPlaces = 1;
+            this.numericUpDown3.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown3.Location = new System.Drawing.Point(17, 247);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(102, 20);
+            this.numericUpDown3.TabIndex = 16;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 563);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PendulumModeling";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+    }
+}
+
---
+++ PendulumModeling/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PendulumModeling
+{
+    public partial class Form1 : Form
+    {
+        // Переменные для визуализации.
+        private Graphics rg;
+        private RectangleF rectangle;
+        private PointF point;
+
+        private const double G = 9.81;    // Ускорение свободного падения.
+        private double k1, k2, k3, k4;    // Переменные для уравнения.
+        private const double STEP = 0.1;  // Шаг для уравнения.
+        private const double _length = 10; // Длина нити.
+
+        private double friction = 0.0;    // Коэффициент трения
+        private double w = 0.0;           // Коэффициент диф. уравнения.
+
+        // Углы для моделирования движения
+        private double omega,
+                       theta;
+
+
+        private double Alpha(double omega, double theta)
+        {
+            //return -(G / _length) * Math.Sin(theta);
+            return -2 * friction * omega - (Math.Pow(w, 2) * Math.Sin(theta));
+        }
+
+
+        private double Theta(double omega)
+        {
+            return omega;
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Решаем диф. уравнение методом Рунге-Кутта 4 порядка
+            k1 = Alpha(omega, theta);
+            k2 = Alpha(omega, theta + (STEP * k1 * 1 / 2));
+            k3 = Alpha(omega, theta + (STEP * k2 * 1 / 2));
+            k4 = Alpha(omega, theta + (STEP * k3));
+            omega += (STEP / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4); // Угловая Скорость
+
+            k1 = Theta(omega);
+            k2 = Theta(omega + (STEP * k1 * 1 / 2));
+            k3 = Theta(omega + (STEP * k2 * 1 / 2));
+            k4 = Theta(omega + (STEP * k3));
+            theta += (STEP / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4); // Угол 
+
+            // Рисуем маятник, для визуализации добавляем координаты формы и множитель.
+            rectangle.X = (float)(Math.Sin(theta) * _length * 20) + pictureBox1.Width / 2;
+            rectangle.Y = (float)(Math.Cos(theta) * _length * 20) + pictureBox1.Height / 2;
+
+            // Рисуем конец линии. Добавляем радиус окружности, чтобы линия крепилась по центру.
+            point.X = rectangle.X + 20;
+            point.Y = rectangle.Y;
+
+            // Перерисовываем контрол с шаром.
+            pictureBox1.Invalidate();
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox3.Text = _length.ToString();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Включаем таймер
+            timer1.Start();
+
+            // Задаем начальные значения переменных при новом старте.
+            // Блокируем кнопки.
+            button1.Enabled = false;
+            rectangle.Y = pictureBox1.Height / 2;
+            rectangle.X = pictureBox1.Width / 2;
+            theta = 5.5;
+            omega = (double)numericUpDown1.Value;//Math.Sqrt(G / _length);
+            friction = (double)numericUpDown2.Value;//Math.Sqrt(G / _length);
+            w = (double)numericUpDown3.Value;//Math.Sqrt(G / _length);
+            button2.Enabled = true;
+            numericUpDown1.Enabled = false;
+            numericUpDown2.Enabled = false;
+            numericUpDown3.Enabled = false;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Останавливаем таймер, разблокируем кнопки.
+            timer1.Stop();
+            button1.Enabled = true;
+            button2.Enabled = false;
+            numericUpDown1.Enabled = true;
+            numericUpDown2.Enabled = true;
+            numericUpDown3.Enabled = true;
+        }
+
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen pen = new Pen(Color.Black, 3); // Цвет и толщина линии.
+            rg = e.Graphics; // Инициализация графики.
+            rg.FillEllipse(Brushes.DeepSkyBlue, rectangle); // Определяем маятник.
+            rg.DrawLine(pen, pictureBox1.Width / 2, 0, point.X, point.Y);        // Рисуем линию.
+            rg.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias; // Добавим сглаживание.
+
+            point = new PointF(pictureBox1.Width / 2, pictureBox1.Height / 2);
+            rectangle = new RectangleF(pictureBox1.Width / 2, pictureBox1.Height / 2, 40, 40);
+        }
+    }
+}
